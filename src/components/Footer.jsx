0853d051dcc040f7f8d74326232ab21b69cf1ea4
Monika <412.kri@gmail.com>
2023-08-07 21:26:33 +0300

    Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 //import style from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 export function Footer() {
     return (
-        <footer>
-            footer...
-        </footer>
+<div className="container">
+  <footer className="py-3 my-4">
+    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+      <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Home</Link></li>
+      <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Features</Link></li>
+      <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Pricing</Link></li>
+      <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">FAQs</Link></li>
+      <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">About</Link></li>
+    </ul>
+    <p className="text-center text-body-secondary">&copy; 2023 Company, Inc</p>
+  </footer>
+</div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 //import style from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 export function Header() {
     const userLoggedIn = true;
@@ -12,15 +13,15 @@ export function Header() {
 
     const userMenu = (
         <div className="dropdown text-end">
-          <a href="/" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link to="/" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"/>
-          </a>
+          </Link>
           <ul className="dropdown-menu text-small">
-            <li><a className="dropdown-item" href="/">New project...</a></li>
-            <li><a className="dropdown-item" href="/">Settings</a></li>
-            <li><a className="dropdown-item" href="/">Profile</a></li>
+            <li><Link className="dropdown-item" to="/">New project...</Link></li>
+            <li><Link className="dropdown-item" to="/">Settings</Link></li>
+            <li><Link className="dropdown-item" to="/">Profile</Link></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Sign out</a></li>
+            <li><Link className="dropdown-item" to="/">Sign out</Link></li>
           </ul>
         </div>
     )
@@ -29,17 +30,17 @@ export function Header() {
     <div className="container">
         <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <div className="col-md-3 mb-2 mb-md-0">
-            <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
+            <Link to="/" className="d-inline-flex link-body-emphasis text-decoration-none">
                 logo
-            </a>
+            </Link>
         </div>
 
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="/" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="/" class="nav-link px-2">Features</a></li>
-            <li><a href="/" class="nav-link px-2">Pricing</a></li>
-            <li><a href="/" class="nav-link px-2">FAQs</a></li>
-            <li><a href="/" class="nav-link px-2">About</a></li>
+            <li><Link to="/" class="nav-link px-2 link-secondary">Home</Link></li>
+            <li><Link to="/" class="nav-link px-2">Features</Link></li>
+            <li><Link to="/" class="nav-link px-2">Pricing</Link></li>
+            <li><Link to="/" class="nav-link px-2">FAQs</Link></li>
+            <li><Link to="/" class="nav-link px-2">About</Link></li>
         </ul>
         
         {userLoggedIn ? userMenu : publicMenu}
