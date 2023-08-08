4209e58b81c08efe4aa1d8f2bd12f87e0ee324cf
Monika <412.kri@gmail.com>
2023-08-08 15:58:37 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BasicLayout } from './layout/BasicLayout';
 import { Home } from './pages/Home';
 import { NoPage } from './pages/NoPage';
+import { Register } from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route Component={BasicLayout}>
-          <Route index path='/' element={<Home/>}  />
+          <Route index path='/' element={<Home/>} />
+          <Route path='/register' element={<Register/>} />
           <Route path='*' element={<NoPage/>} />
         </Route>
       </Routes>
