005598a80ce6e6f5dfddfc545793d6265f6769f9
Monika <412.kri@gmail.com>
2023-08-09 21:51:23 +0300

    validation ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,21 +7,21 @@ export function Login() {
     <form>
         <h1 className="hą mb-3 fw-normal">Please log in</h1>
         <div className="form-floating mb-3">
-            <input type="email" class="form-control" id="email" placeholder="name@example.com"/>
-            <label for="floatingInput">Email address</label>
+            <input type="email" className="form-control" id="email" placeholder="name@example.com"/>
+            <label htmlFor="floatingInput">Email address</label>
         </div>
         <div className="form-floating mb-3">
-            <input type="password" class="form-control" id="password" placeholder="Password"/>
-            <label for="floatingPassword">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Password"/>
+            <label htmlFor="floatingPassword">Password</label>
         </div>
         <div className="form-check text-start my-3">
             <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
-            <label className="form-check-label" for="flexCheckDefault">
+            <label className="form-check-label" htmlFor="flexCheckDefault">
             Agree to <Link to='/'>Terms of Service</Link>
         </label>
         </div>
-        <Link to='/' className="btn btn-primary w-100 py-2 mb-3" type="submit">Sign in</Link>
-        <Link to='/register' className="btn btn-outline-primary w-100 py-2" type="submit">Sign up</Link>
+        <button className="btn btn-primary w-100 py-2 mb-3" type="submit">Sign in</button>
+        <Link to='/register' className="btn btn-outline-primary w-100 py-2">Sign up</Link>
         <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
     </form>
 </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,35 +1,146 @@
 import style from './Register.module.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export function Register() {
+
+    const [username, setUsername] = useState('');
+    const [usernameErr, setUsernameErr] = useState('false');
+    const [usernameValid, setUsernameValid] = useState(false);
+
+    const [email, setEmail] = useState('');
+    const [emailErr, setEmailErr] = useState('false');
+    const [emailValid, setEmailValid] = useState(false);
+
+    const [pass, setPass] = useState('');
+    const [passErr, setPassErr] = useState('false');
+    const [passValid, setPassValid] = useState(false);
+
+    const [repass, setRepass] = useState('');
+    const [repassErr, setRepassErr] = useState('false');
+    const [repassValid, setRepassValid] = useState(false);
+
+    function updateUsername(e) {
+        setUsername(e.target.value);
+    }
+
+    function updateEmail(e) {
+    setEmail(e.target.value);
+    }
+
+    function updatePass(e) {
+    setPass(e.target.value);
+    }
+
+    function updateRepass(e) {
+    setRepass(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        const minUsernameSize = 2;
+        const minEmailSize = 6;
+        const minPassSize = 6;
+
+        if (username.length < minUsernameSize) {
+            setUsernameErr(true);
+            setUsernameValid(false);
+        } else {
+            setUsernameErr(false);
+            setUsernameValid(true);
+        }
+
+        if (email.length < minEmailSize) {
+            setEmailErr(true);
+            setEmailValid(false);
+        } else {
+            setEmailErr(false);
+            setEmailValid(true);
+        }
+
+        if (pass.length < minPassSize) {
+            setPassErr(true);
+            setPassValid(false);
+        } else {
+            setPassErr(false);
+            setPassValid(true);
+        }
+
+        if (pass !== repass) {
+            setRepassErr(true);
+            setRepassValid(false);
+        } else {
+            setRepassErr(false);
+            setRepassValid(true);
+        }
+
+        if (!usernameErr && !emailErr && !passErr && !repassErr) {
+            console.log('siunciam i serveri...');
+        }
+    }
+
     return (
 <div className={`form-signin w-100 m-auto ${style.formSignin}`}>
-    <form>
-        <h1 className="hą mb-3 fw-normal">Please sign up</h1>
+    <form onSubmit={handleSubmit}>
+        <h1 className="h1 mb-3 fw-normal">Please sign up</h1>
+
         <div className="form-floating mb-3">
-            <input type="text" class="form-control" id="username" placeholder="name@example.com"/>
-            <label for="floatingInput">Username</label>
+            <input onChange={updateUsername} type="text" id="username"
+            className={`form-control ${usernameErr ? 'is-invalid' : ''} ${usernameValid ? 'is-valid' : ''}`} />
+            <label htmlFor="floatingInput">Username</label>
+            <div className="valid-feedback">
+                Looks good!
+            </div>
+            <div className="invalid-feedback">
+                Please choose username.
+            </div>
         </div>
+        
         <div className="form-floating mb-3">
-            <input type="email" class="form-control" id="email" placeholder="name@example.com"/>
-            <label for="floatingInput">Email address</label>
+            <input onChange={updateEmail} type="email" id="email"
+            className={`form-control ${emailErr ? 'is-invalid' : ''} ${emailValid ? 'is-valid' : ''}`} />
+            <label htmlFor="floatingInput">Email address</label>
+            <div className="valid-feedback">
+                Looks good!
+            </div>
+            <div className="invalid-feedback">
+                Please write email.
+            </div>
         </div>
+
         <div className="form-floating mb-3">
-            <input type="password" class="form-control" id="password" placeholder="Password"/>
-            <label for="floatingPassword">Password</label>
+            <input onChange={updatePass} type="password" id="password"
+            className={`form-control ${passErr ? 'is-invalid' : ''} ${passValid ? 'is-valid' : ''}`} />
+            <label htmlFor="floatingPassword">Password</label>
+            <div className="valid-feedback">
+                Looks good!
+            </div>
+            <div className="invalid-feedback">
+                Please create password.
+            </div>
         </div>
+
         <div className="form-floating mb-3">
-            <input type="password" class="form-control" id="repass" placeholder="Password"/>
-            <label for="floatingPassword">Repeat password</label>
+            <input onChange={updateRepass} type="password" id="repass"
+            className={`form-control ${repassErr ? 'is-invalid' : ''} ${repassValid ? 'is-valid' : ''}`} />
+            <label htmlFor="floatingPassword">Repeat password</label>
+            <div className="valid-feedback">
+                Looks good!
+            </div>
+            <div className="invalid-feedback">
+                Please repeat same password.
+            </div>
         </div>
+
         <div className="form-check text-start my-3">
             <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
-            <label className="form-check-label" for="flexCheckDefault">
+            <label className="form-check-label" htmlFor="flexCheckDefault">
             Remember me
         </label>
         </div>
-        <Link to='/' className="btn btn-primary w-100 py-2 mb-3" type="submit">Sign up</Link>
-        <Link to='/login' className="btn btn-outline-primary w-100 py-2" type="submit">Sign in</Link>
+        <button className="btn btn-primary w-100 py-2 mb-3" type="submit">Sign up</button>
+        <Link to='/login' className="btn btn-outline-primary w-100 py-2">Sign in</Link>
         <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
     </form>
 </div>
